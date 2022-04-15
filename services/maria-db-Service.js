b0d58aee883e1420e4516d2d3e660b2334cb7974
Tal Ben David <talbd51@gmail.com>
2022-04-15 15:19:36 +0300

    changed host adress

--- services/maria-db-Service.js
+++ services/maria-db-Service.js
@@ -2,7 +2,7 @@ const mariadb = require('mariadb');
 // const { dbPassword } = require('../secrets')
 //metabook.cu3syqqzdyfg.us-east-1.rds.amazonaws.com
 const pool = mariadb.createPool({
-    host: 'metabook.cu3syqqzdyfg.us-east-1.rds.amazonaws.com',
+    host: 'metabook.csx5zg5uetwq.eu-west-1.rds.amazonaws.com',
     user: 'wordchained',
     password: process.env.dbPassword,
     connectionLimit: 100,
