ed8c44273a7813a59725d9c8370b03bd001e12b0
Tal Ben David <talbd51@gmail.com>
2022-04-21 12:29:03 +0300

    .

--- services/socket-service.js
+++ services/socket-service.js
@@ -4,7 +4,7 @@ const logger = require('./logger-service');
 const socketService = (server, session) => {
     const io = socketIo(server, {
         cors: {
-            origin: ["http://localhost:3000", 'http://127.0.0.1:3000', 'https://git.heroku.com/metabook-wordchained-frontend', "http://localhost:3030", "https://wordchained.github.io/metabook/", " https://metabook-wordchained-frontend.herokuapp.com"],
+            origin: ["http://localhost:3000", 'http://127.0.0.1:3000', 'https://git.heroku.com/metabook-wordchained-frontend', "http://localhost:3030", "https://wordchained.github.io/metabook/", "https://metabook-wordchained-frontend.herokuapp.com"],
             credentials: true,
         }
     });
